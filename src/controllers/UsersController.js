1f1cb29ccb58ad5511cb8187fe3bb150ec934bfe
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-04-17 08:58:55 -0300

    create update method in UsersController.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -19,6 +19,45 @@ class UsersController {
 
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const { name, email, phone, birthday, hometown } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name;
+    user.email = email;
+    user.phone = phone;
+    user.birthday = birthday;
+    user.hometown = hometown;
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      phone = ?,
+      birthday = ?,
+      hometown = ?,
+      updated_at = ?
+
+      WHERE id = ?`,
+      [user.name, user.email, user.phone, user.birthday, user.hometown, new Date(), id]
+    );
+
+    return response.status(200).json();
+  }
 }
 
 module.exports = UsersController;

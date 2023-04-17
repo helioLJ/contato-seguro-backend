9a726a9c8183afdca77af3f26518a6e255ac3a5a
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-04-17 06:41:03 -0300

    first route test, install nodemon and create script run dev

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));

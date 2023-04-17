12bbf8fe188445c099c5dc80663d2ca800286af3
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-04-17 07:14:31 -0300

    create folder utils; create middleware AppError; install express-async-erros; use AppError

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
   create(request, response) {
     const { name, email, phone, birthday, hometown } = request.body;
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório.")
+    }
 
     response.json({ name, email, phone, birthday, hometown });
   }
